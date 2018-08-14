f7a69c04f1fa4090738a2b1a1889ad99b4d1478a
BJ Pennington <bjpennington112@gmail.com>
2018-08-13 22:43:01 -0500

    create server side get route for classrooms with server side authentication

--- server/routes/classroom.router.js
+++ server/routes/classroom.router.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const {rejectUnauthenticated} = require('../modules/authentication-middleware')
 const router = express.Router();
 
 const sampleClassrooms = [
     {id: 1, classroom_name: 'Physical Science Hour 1', person_id: 1}, 
     {id: 2, classroom_name: 'Engineering Hour 2', person_id: 1},
     {id: 3, classroom_name: 'Life Science Hour 3', person_id: 1},
-    {id: 3, classroom_name: 'Physical Science Hour 4', person_id: 1},
+    {id: 4, classroom_name: 'Physical Science Hour 4', person_id: 1},
 ]
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+    res.send(sampleClassrooms);
+});
 
 module.exports = router;
