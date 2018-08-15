8bec1e7f7f7e3089cf1a4b95dcb04c336c9e9cb9
BJ Pennington <bjpennington112@gmail.com>
2018-08-15 15:49:02 -0500

    basic table of standards and students created

--- src/components/ScoresTable/ScoresTable.jsx
+++ src/components/ScoresTable/ScoresTable.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 
 import { CLASSROOM_ACTIONS } from '../../redux/actions/classroomActions';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
+import {STANDARD_ACTIONS} from '../../redux/actions/standardActions';
+import {STUDENT_ACTIONS} from '../../redux/actions/studentActions';
 
 import Nav from '../Nav/Nav';
 
@@ -11,6 +13,14 @@ class ScoresTable extends Component {
         this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
         this.props.dispatch({
             type: CLASSROOM_ACTIONS.FETCH_CURRENT_CLASSROOM,
+            payload: this.props.match.params.id,
+        });
+        this.props.dispatch({
+            type: STANDARD_ACTIONS.FETCH_STANDARD,
+            payload: this.props.match.params.id,
+        });
+        this.props.dispatch({
+            type: STUDENT_ACTIONS.FETCH_STUDENT,
             payload: this.props.match.params.id,
         });
     }
@@ -30,17 +40,44 @@ class ScoresTable extends Component {
     }
 
     render() {
-        console.log('scores props:', this.props.match);
         
+        let standardsMapArray = this.props.standards.map((standard, index) => {
+            return(
+                <th key={index}>{standard.standard_name}</th>
+            )
+        });
+
+        let studentsMapArray = this.props.students.map((student, index) => {
+            return (
+                <tr key={index}>
+                    <td>{student.student_name}</td>
+                </tr>
+            )
+        })
 
         let content = null;
 
         if (this.props.user.userName) {
             content = (
+
                 <div>
+                    {JSON.stringify(this.props.standards)}
+                    {JSON.stringify(this.props.students)}
+                    <h3>{this.props.currentClassroom.classroom_name}</h3>
                     <p>
                         Scores Table
                     </p>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                {standardsMapArray}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {studentsMapArray}
+                        </tbody>
+                    </table>
                     <button onClick={this.editClassroom}>
                         Edit Classroom
                     </button>
@@ -62,6 +99,9 @@ class ScoresTable extends Component {
 
 const mapStateToProps = state => ({
     user: state.user,
+    currentClassroom: state.classroom.currentClassroom,
+    standards: state.standard.standards,
+    students: state.student.students,
 });
 
 export default connect(mapStateToProps)(ScoresTable);
