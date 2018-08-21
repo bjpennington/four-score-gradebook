d8551454217f99ce61d54c515cb38099f7409bd5
BJ Pennington <bjpennington112@gmail.com>
2018-08-21 15:23:48 -0500

    add sweet alert confirmation for deleting standard

--- src/components/StandardListItem/StandardListItem.jsx
+++ src/components/StandardListItem/StandardListItem.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {STANDARD_ACTIONS} from '../../redux/actions/standardActions';
+import swal from 'sweetalert';
 
 class StandardListItem extends Component {
 
     deleteStandard = () => {
-        this.props.dispatch({
-            type: STANDARD_ACTIONS.DELETE_STANDARD,
-            payload: this.props.standard.id
+        swal({
+            title: `Delete "${this.props.standard.standard_name}"?`,
+            text: `Are you sure you want to delete "${this.props.standard.standard_name}"? All scores associated with this standard will also be deleted. This cannot be undone.`,
+            icon: "warning",
+            dangerMode: true,
+            buttons: [true, "Delete"],
         })
+            .then(confirmed => {
+                if (confirmed) {
+                    this.props.dispatch({
+                        type: STANDARD_ACTIONS.DELETE_STANDARD,
+                        payload: this.props.standard.id
+                    });
+                }
+            });
     }
 
     render() {
