ede0162b09469c5dc9897d5ce4a7b72706dcd0eb
BJ Pennington <bjpennington112@gmail.com>
2018-08-22 15:58:07 -0500

    add logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const Header = ({ title }) => (
-  <div className="instructions">
-    <div>
-      <h1 className="lead">{title}</h1>
+    <div className="instructions">
+      <div style={{backgroundColor: "#f44336", height: 71}}><img src="/images/eco-green-apple-icon-152-183645.png" alt="fourscore-logo" height="71" /></div>
+      <h1>{title}</h1>
     </div>
-  </div>
 );
 
 export default Header;
